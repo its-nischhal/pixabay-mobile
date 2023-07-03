86fd6d9a2a20b807c9f134033d8eaddfe21cc474
Nischhal Sharma <to.nichhal@gmail.com>
2023-07-04 02:21:23 +0530

    [Add] Routes

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,3 +1,22 @@
+import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'package:pixabay_mobile/app/modules/home_page/home_page.dart';
+import 'package:pixabay_mobile/app/modules/home_page/home_page_binding.dart';
+import 'package:pixabay_mobile/app/modules/splash_page/home_page_binding.dart';
+import 'package:pixabay_mobile/app/modules/splash_page/splash_page.dart';
+
+import 'app_routes.dart';
+
 class AppPages{
-  
+    static final List<GetPage> pages = [
+    GetPage(
+      name: AppRoutes.splashPage,
+      page: () => const SplashPage(),
+      binding: SplashPageBinding(),
+    ),
+    GetPage(
+      name: AppRoutes.homePage,
+      page: () => const HomePage(),
+      binding: HomePageBinding(),
+    ),
+  ];
 }
