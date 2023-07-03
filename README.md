344f2aae88d9d29bc931f3d96227c0f001845402
Nischhal Sharma <to.nichhal@gmail.com>
2023-07-03 18:42:28 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pixabay_mobile
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/pixabay_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pixabay_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
